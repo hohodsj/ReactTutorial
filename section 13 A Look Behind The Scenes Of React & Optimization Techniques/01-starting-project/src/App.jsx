ba72065c61c4eb8e4ce925702b82802723df9d5e
hohodsj <hohodsj@gmail.com>
2025-05-02 23:05:14 -0400

    set prime and value propagate to Counter.jsx

--- section 13 A Look Behind The Scenes Of React & Optimization Techniques/01-starting-project/src/App.jsx
+++ section 13 A Look Behind The Scenes Of React & Optimization Techniques/01-starting-project/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCount} />
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
         <Counter initialCount={0} />
       </main>
     </>
