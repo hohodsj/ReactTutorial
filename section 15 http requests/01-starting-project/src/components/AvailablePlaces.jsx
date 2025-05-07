98bb69de91c46221f66c13829439edc11eca51a6
hohodsj <hohodsj@gmail.com>
2025-05-06 22:52:07 -0400

    update await async syntax

--- section 15 http requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ section 15 http requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -5,13 +5,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const data = await response.json();
       setAvailablePlaces(data.places);
-    });
+    }
+    fetchPlaces();
   }, [])
 
   return (
