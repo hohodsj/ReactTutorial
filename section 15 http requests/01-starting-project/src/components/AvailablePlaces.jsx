a61b07ea9d53792fd20dc7cc61f567d698bcdc02
hohodsj <hohodsj@gmail.com>
2025-05-10 09:52:39 -0400

    move fetch to http.js

--- section 15 http requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ section 15 http requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
 import ErrorMessage from './ErrorMessage.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import fetchAvailablePlaces from '../http.js'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -12,14 +13,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const data = await response.json();
-        if (!response.ok) {
-          throw new Error('Error fetching places'); 
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(data.places, position.coords.latitude, position.coords.longitude);
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false); // We setIsFetching because callback is async
         });
---
+++ section 15 http requests/01-starting-project/src/http.js
@@ -0,0 +1,8 @@
+export default async function fetchAvailablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error('Error fetching places'); 
+    }
+    return data.places;
+}
