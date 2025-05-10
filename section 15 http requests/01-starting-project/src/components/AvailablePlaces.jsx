4efa83a2bb56e7f684665214125a0f843c65e0f2
hohodsj <hohodsj@gmail.com>
2025-05-10 11:02:16 -0400

    post req example

--- section 15 http requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ section 15 http requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -2,7 +2,7 @@ import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
 import ErrorMessage from './ErrorMessage.jsx';
 import { sortPlacesByDistance } from '../loc.js';
-import fetchAvailablePlaces from '../http.js'
+import {fetchAvailablePlaces} from '../http.js'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
