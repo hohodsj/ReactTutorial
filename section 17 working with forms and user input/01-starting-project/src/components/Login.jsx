31dea60c560f531a27199ad0e8c97432209def68
hohodsj <hohodsj@gmail.com>
2025-05-19 22:13:51 -0400

    handle input

--- section 17 working with forms and user input/01-starting-project/src/components/Login.jsx
+++ section 17 working with forms and user input/01-starting-project/src/components/Login.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 export default function Login() {
-    function handleSubmit(event) {
-      event.preventDefault();
-      console.log("Form submitted");
+    // approach 1: use 2 states
+    /*
+    const [enteredEmail, setEnteredEmail] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState("");
+
+    function handleEmailChange(event) {
+        setEnteredEmail(event.target.value);
+        console.log("Email changed", event.target.value);
     }
+    
+    function handlePasswordChange(event) {
+        setEnteredPassword(event.target.value);
+        console.log("Password changed", event.target.value);
+    }
+   */
+
+    // approach 2: use 1 state
+    const [enteredValues, setEnteredValues] = useState({});
+    function handleInputChange(identifier, event) {
+        setEnteredValues((prevState) => ({
+            ...prevState,
+            [identifier]: event.target.value,
+        }));
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Form submitted");
+    }
+
     return (
         <form onSubmit={handleSubmit}>
             <h2>Login</h2>
@@ -10,20 +38,32 @@ export default function Login() {
             <div className="control-row">
                 <div className="control no-margin">
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" />
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        onChange={(event) => handleInputChange("email", event)}
+                        value={enteredValues.email || ""}
+                    />
                 </div>
 
                 <div className="control no-margin">
-                    <label htmlFor="password">Password</label>
+                    <label
+                        htmlFor="password"
+                        onChange={(event) =>
+                            handleInputChange("password", event)
+                        }
+                        value={enteredValues.password || ""}
+                    >
+                        Password
+                    </label>
                     <input id="password" type="password" name="password" />
                 </div>
             </div>
 
             <p className="form-actions">
                 <button className="button button-flat">Reset</button>
-                <button className="button">
-                    Login
-                </button>
+                <button className="button">Login</button>
             </p>
         </form>
     );
