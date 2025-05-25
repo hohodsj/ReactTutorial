6d4007a450c6088b650da524d280189c9b2eb2c7
hohodsj <hohodsj@gmail.com>
2025-05-25 19:21:14 -0400

    add custom logic for form validation

--- section 17 working with forms and user input/01-starting-project/src/components/Signup.jsx
+++ section 17 working with forms and user input/01-starting-project/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+    const [passwordAreNotEqual, setPasswordAreNotEqual] = useState(false);
+
     function handleSubmit(event) {
         event.preventDefault();
         // FormData is a built-in browser API not a React API, to access must have name attributes in the form
@@ -8,6 +12,11 @@ export default function Signup() {
         const data = Object.fromEntries(fd.entries()); // convert the FormData object to a regular object
         data.acquisition = acquisitionChannel;
         console.log("Form submitted", data);
+        if(data.password !== data["confirm-password"]) // name field called password that why we can use data.password, becuase it doesn't support -, we need to use data["confirm-password"]
+        {
+            setPasswordAreNotEqual(true);
+            return;
+        }
 
         event.target.reset();
     }
@@ -37,6 +46,11 @@ export default function Signup() {
                         type="password"
                         name="confirm-password"
                     />
+                    <div className="control-error">
+                        {passwordAreNotEqual && (
+                            <p>Passwords do not match</p>
+                        )}
+                    </div>
                 </div>
             </div>
 
