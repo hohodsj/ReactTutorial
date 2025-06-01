8d46747445a755fbcf222e3b24a0b2f0b63f42ab
hohodsj <hohodsj@gmail.com>
2025-05-31 22:59:11 -0400

    add react-toolkit

--- section 20 diving into redux/01-starting-project/src/store/index.js
+++ section 20 diving into redux/01-starting-project/src/store/index.js
@@ -1,34 +1,29 @@
 import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "increment":
-            return {
-                counter: state.counter + 1,
-                showCounter: state.showCounter,
-            };
-        case "increase":
-            return {
-                counter: state.counter + action.value,
-                showCounter: state.showCounter,
-            };
-        case "decrement":
-            return {
-                counter: state.counter - 1,
-                showCounter: state.showCounter,
-            };
-        case "toggle":
-            return {
-                ...state, // spread operator to keep the current state
-                showCounter: !state.showCounter,
-            }; // toggle the showCounter boolean
-        default:
-            return state;
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducer: {
+        increment(state){
+            state.counter++; // directly mutate the state, Redux Toolkit uses Immer under the hood, and change the behavior to copy state and update counter.
+        },
+        decrement(state){
+            state.counter--; 
+        },
+        increase(state, action){
+            state.counter += action.value; // action.value is the payload passed when dispatching the action
+        },
+        toggle(state){
+            state.showCounter = !state.showCounter; // toggle the showCounter boolean
+        },
     }
-};
+})
 
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
 export default store;
