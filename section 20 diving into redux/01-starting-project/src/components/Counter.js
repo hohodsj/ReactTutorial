a4113b5020cd098c90ec6a4057ec08fe6c574514
hohodsj <hohodsj@gmail.com>
2025-05-31 22:01:36 -0400

    manage multiple states

--- section 20 diving into redux/01-starting-project/src/components/Counter.js
+++ section 20 diving into redux/01-starting-project/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch(); // setup redux dispatch
   const counter = useSelector(state => state.counter); // setup redux subscription
+  const show = useSelector(state => state.showCounter); // subscribe to showCounter state
   
   const incrementHandler = () => {
     dispatch({ type: 'increment'}); // actoun to increase the counter must match with string in /src/store/index.js
@@ -14,12 +15,14 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: 'decrement'}); 
   }
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'}); // action to toggle the visibility of the counter
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show &&<div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button> {/* This button will dispatch an action to increment the counter  */}
         <button onClick={increaseHandler}>Increment By 5</button>
--- section 20 diving into redux/01-starting-project/src/store/index.js
+++ section 20 diving into redux/01-starting-project/src/store/index.js
@@ -1,14 +1,25 @@
 import {createStore} from 'redux';
 
+const initialState = {counter: 0, showCounter: true};
 
-const counterReducer = (state = {counter : 0}, action) => {
+const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case "increment":
-            return { counter: state.counter + 1 };
+            return { 
+                counter: state.counter + 1,
+                showCounter: state.showCounter};
         case "increase":
-            return { counter: state.counter + action.value}
+            return { 
+                counter: state.counter + action.value,
+                showCounter: state.showCounter };
         case "decrement":
-            return { counter: state.counter - 1 };
+            return { 
+                counter: state.counter - 1,
+            showCounter: state.showCounter };
+        case "toggle":
+            return { 
+                ...state, // spread operator to keep the current state
+                showCounter: !state.showCounter }; // toggle the showCounter boolean
         default:
             return state;
     }
