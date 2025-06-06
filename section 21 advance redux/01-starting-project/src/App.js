af1cd271da9d81d3f066e4b077324a24f653623e
hohodsj <hohodsj@gmail.com>
2025-06-05 23:10:13 -0400

    add fetch in redux

--- section 21 advance redux/01-starting-project/src/App.js
+++ section 21 advance redux/01-starting-project/src/App.js
@@ -2,9 +2,16 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch('https://react-http-1234-default-rtdb.firebaseio.com/cart.json', {method: 'PUT', body: JSON.stringify(cart)})
+  }, [cart])
   return (
     <Layout>
       {showCart && <Cart />}
