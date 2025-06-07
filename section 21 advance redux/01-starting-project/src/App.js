fa8cfbab1eb2abf1bfc2026d209d2705b264d007
hohodsj <hohodsj@gmail.com>
2025-06-06 23:35:38 -0400

    add notification ui with useEffect

--- section 21 advance redux/01-starting-project/src/App.js
+++ section 21 advance redux/01-starting-project/src/App.js
@@ -1,23 +1,75 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 // import { initializeApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.cart);
-  useEffect(() => {
-    fetch('https://react-http-1234-default-rtdb.firebaseio.com/cart.json', {method: 'PUT', body: JSON.stringify(cart)})
-  }, [cart])
-  return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
+    const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+    const notification = useSelector((state) => state.ui.notification);
+
+    useEffect(() => {
+        const sendCartData = async () => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "pending",
+                    title: "Sending...",
+                    message: "Sending cart data!",
+                })
+            );
+            const response = await fetch(
+                "https://react-http-1234-default-rtdb.firebaseio.com/cart.json",
+                { method: "PUT", body: JSON.stringify(cart) }
+            );
+            if (!response.ok) {
+                throw new Error("Sending cart data failed.");
+            }
+            dispatch(
+                uiActions.showNotification({
+                    status: "success",
+                    title: "Success!",
+                    message: "Sent cart data successfully!",
+                })
+            );
+        };
+        if(isInitial) {
+            isInitial = false;
+            return;
+        }
+        sendCartData().catch((error) => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "error",
+                    title: "Error!",
+                    message: "Sending cart data failed!",
+                })
+            );
+        });
+    }, [cart, dispatch]);
+    return (
+        <>
+            {notification && (
+                <Notification
+                    status={notification?.status}
+                    title={notification?.title}
+                    message={notification?.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </>
+    );
 }
 
 export default App;
