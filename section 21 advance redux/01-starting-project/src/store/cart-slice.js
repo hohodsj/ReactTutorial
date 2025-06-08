df590d6f2f76e90bc1e18eb879ba5ab6f5a889fd
hohodsj <hohodsj@gmail.com>
2025-06-07 22:58:09 -0400

    action creator

--- section 21 advance redux/01-starting-project/src/store/cart-slice.js
+++ section 21 advance redux/01-starting-project/src/store/cart-slice.js
@@ -1,22 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
-    name: 'cart',
+    name: "cart",
     initialState: {
         items: [],
         totalQuantity: 0,
     },
     reducers: {
-        addItemToCart(state, action){
+        addItemToCart(state, action) {
             const newItem = action.payload;
-            const existingItem = state.items.find((item) => item.itemId === newItem.id);
-            if(!existingItem) {
+            const existingItem = state.items.find(
+                (item) => item.itemId === newItem.id
+            );
+            if (!existingItem) {
                 state.items.push({
-                    itemId: newItem.id, 
-                    price: newItem.price, 
-                    quantity: 1, 
+                    itemId: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
                     totalPrice: newItem.price,
-                    name: newItem.name
+                    name: newItem.name,
                 });
             } else {
                 existingItem.quantity++;
@@ -27,16 +30,55 @@ const cartSlice = createSlice({
         removeItemFromCart(state, action) {
             const id = action.payload;
             const existingItem = state.items.find((item) => item.itemId === id);
-            if(existingItem.quantity === 1) {
+            if (existingItem.quantity === 1) {
                 state.items = state.items.filter((item) => item.itemId !== id);
             } else {
                 existingItem.quantity--;
                 existingItem.totalPrice -= existingItem.price;
             }
             state.totalQuantity--;
+        },
+    },
+});
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(
+            uiActions.showNotification({
+                status: "pending",
+                title: "Sending...",
+                message: "Sending cart data!",
+            })
+        );
+        const sendRequest = async () => {
+            const response = await fetch(
+                "https://react-http-1234-default-rtdb.firebaseio.com/cart.json",
+                { method: "PUT", body: JSON.stringify(cart) }
+            );
+            if (!response.ok) {
+                throw new Error("Sending cart data failed.");
+            }
+        };
+        try {
+            await sendRequest();
+            dispatch(
+                uiActions.showNotification({
+                    status: "success",
+                    title: "Success!",
+                    message: "Sent cart data successfully!",
+                })
+            );
+        } catch (error) {
+            dispatch(
+                uiActions.showNotification({
+                    status: "error",
+                    title: "Error!",
+                    message: "Sending cart data failed!",
+                })
+            );
         }
-    }
-})
+    };
+};
 
 export default cartSlice;
 export const cartActions = cartSlice.actions;
