6c92ca997499e6dc761dc77cfac47c2087aef1a0
hohodsj <hohodsj@gmail.com>
2025-07-04 23:02:40 -0400

    mock fetch

--- section 31 testing react apps/01-starting-project/src/components/Async.test.js
+++ section 31 testing react apps/01-starting-project/src/components/Async.test.js
@@ -3,6 +3,11 @@ import Async from './Async';
 
 describe('Async component', () => {
     test('renders posts if reuqest succeeds', async () => {
+        window.fetch = jest.fn(); // jest.fn() creates a mock function
+        // mock the fetch function to return a resolved promise with the expected data
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'First Post'}]
+        });
         render(<Async />);
 
         // if only getByRole and multiple items, it would fail
